c814a2fba62695dc1e226fe1baa25f67da15a9a6
faresSultan <fares.sultan9@gmail.com>
2024-11-20 17:34:31 +0200

    tokenizedXML done, but still under testing

--- src/functions.cpp
+++ src/functions.cpp
@@ -4,5 +4,22 @@ vector<string> tokenizeXML (string XML_data){
     vector <string> TokenizedXML;
     string tag = 0;
 
-    for(int i = 0 )
+    for(int i = 0 ; i<XML_data.size();i++){
+
+        if(XML_data[i] == '<'){
+            tag.push_back(XML_data[i]);
+            int j =i+1;
+            while (XML_data[j]!='>')
+            {
+                tag.push_back(XML_data[j]);
+            }
+            if(XML_data[j] == '>'){
+                tag.push_back(XML_data[j]);
+            }
+           i = j;
+           TokenizedXML.push_back(tag);
+           tag.clear();  
+        }
+    }
+    return TokenizedXML;
 }
