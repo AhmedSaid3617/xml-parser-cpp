3caf56fdb47bbd24ffd5a47506830bce1872846b
Abdelrahman Sherif <abdelrahmansherif599@gmail.com>
2024-11-25 09:17:28 +0200

    add the tree architicture

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,97 @@
 #include <unordered_map>
 
 using namespace std;
+
+
+class Node {
+public:
+    string name;                   // Tag name
+    string content;                // Text content inside the tag
+    vector<Node*> children;        // List of child nodes
+
+    // Constructor
+    Node(const string& nodeName) : name(nodeName), content("") {}
+
+    ~Node() { // Destructor to clean up children
+        for (Node* child : children) {
+            delete child;
+        }
+    }
+};
+
+class Tree {
+public:
+    Node* root;
+
+    Tree() : root(nullptr) {}
+
+    void build(const vector<string>& tokens) {
+        root = parseXMLTree(tokens);
+    }
+
+
+    ~Tree() {
+        delete root;
+    }
+
+private:
+    Node* parseXMLTree(const vector<string>& tokens) {
+        Node* root = nullptr;
+        stack<Node*> nodeStack;
+
+        for (const auto& token : tokens) {
+            if (token[0] == '<' && token[1] != '/') { // Opening tag or self-closing tag
+                size_t nameEnd = token.find_first_of(" />", 1);
+                string tagName = token.substr(1, nameEnd - 1);
+
+                Node* newNode = new Node(tagName);
+
+                if (!nodeStack.empty()) {
+                    nodeStack.top()->children.push_back(newNode);
+                } else {
+                    root = newNode;
+                }
+
+                if (token.back() != '/') { // Not self-closing tag
+                    nodeStack.push(newNode);
+                }
+            } else if (token[0] == '<' && token[1] == '/') { // Closing tag
+                nodeStack.pop();
+            } else { // Text content
+                if (!nodeStack.empty()) {
+                    nodeStack.top()->content = token;
+                }
+            }
+        }
+
+        return root;
+    }
+
+
+
+
+};
+vector<string> tokenizeXML(const string& xml) {
+    vector<string> tokens;
+    size_t pos = 0;
+
+    while (pos < xml.length()) {
+        size_t start = xml.find('<', pos);
+        if (start == string::npos) break;
+
+        if (start > pos) { // Text content
+            tokens.push_back(xml.substr(pos, start - pos));
+        }
+
+        size_t end = xml.find('>', start);
+        if (end == string::npos) break;
+
+        tokens.push_back(xml.substr(start, end - start + 1));
+        pos = end + 1;
+    }
+
+    return tokens;
+}
 // Helper to find the next occurrence of a character
 int find(int indx, char c, const string& line) {
     for (int i = indx; i < line.size(); i++) {
