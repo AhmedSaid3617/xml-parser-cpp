41ff84e857080025c4a87ebeff4506df8350cd6a
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-11-28 16:25:55 +0200

    Added convert to json to GUI.

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include "consistency/consistency.h"
+#include "convert/tree_to_json.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow)
@@ -20,7 +21,8 @@ void MainWindow::on_actionOpen_triggered()
         return;
     QFile file(fileName);
     currentFileName = fileName;
-    if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
+    if (!file.open(QIODevice::ReadOnly | QFile::Text))
+    {
         QMessageBox::warning(this, "Warning", "Cannot open file: " + file.errorString());
         return;
     }
@@ -31,28 +33,30 @@ void MainWindow::on_actionOpen_triggered()
     file.close();
 }
 
-
 void MainWindow::on_actionNew_triggered()
 {
     currentFileName.clear();
     ui->textEdit->setText(QString());
 }
 
-
 void MainWindow::on_actionSave_triggered()
 {
     QString fileName;
     // If we don't have a filename from before, get one.
-    if (currentFileName.isEmpty()) {
+    if (currentFileName.isEmpty())
+    {
         fileName = QFileDialog::getSaveFileName(this, "Save");
         if (fileName.isEmpty())
             return;
         currentFileName = fileName;
-    } else {
+    }
+    else
+    {
         fileName = currentFileName;
     }
     QFile file(fileName);
-    if (!file.open(QIODevice::WriteOnly | QFile::Text)) {
+    if (!file.open(QIODevice::WriteOnly | QFile::Text))
+    {
         QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
         return;
     }
@@ -62,7 +66,6 @@ void MainWindow::on_actionSave_triggered()
     out << text;
     file.close();
 }
-
 
 void MainWindow::on_actionCheck_for_errors_triggered()
 {
@@ -79,14 +82,34 @@ void MainWindow::on_actionCheck_for_errors_triggered()
     }
 
     // TODO: Remove whitespaces but not new lines.
-    
+
     ui->outputLabel->setText(out_string);
-
 }
-
 
 void MainWindow::on_actionFormat_triggered()
 {
-
 }
 
+void MainWindow::on_actionConvert_to_JSON_triggered()
+{
+    QString fileName;
+    // If we don't have a filename from before, get one.
+
+    fileName = QFileDialog::getSaveFileName(this, "Save");
+    if (fileName.isEmpty())
+        return;
+    currentFileName = fileName;
+
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly | QFile::Text))
+    {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text;
+    text = xml_to_json(ui->textEdit->toPlainText().toStdString()).c_str();
+    out << text;
+    file.close();
+}
--- src/gui/mainwindow.h
+++ src/gui/mainwindow.h
@@ -32,6 +32,8 @@ private slots:
 
     void on_actionFormat_triggered();
 
+    void on_actionConvert_to_JSON_triggered();
+
 private:
     Ui::MainWindow *ui;
     QString currentFileName;
