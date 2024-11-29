c3360be78a7fcf6c4bc4bbebfd2979a560ae7463
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-11-29 15:21:22 +0200

    Moved format.cpp to its own directory.

--- src/console/main.cpp
+++ src/console/main.cpp
@@ -1,7 +1,10 @@
 #include <string>
+#include <iostream>
+#include "fromat/format.h"
 
 int main() {
     std::string my_string = "<id><x></x></id>";
+    std::cout << processXML(my_string, "out.txt");
 
     return 0;
 }
