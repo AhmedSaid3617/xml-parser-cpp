98ab29a7f7c9955c206074c8a611868b06b8569c
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-11-29 15:05:56 +0200

    Added header file for format.cpp

---
+++ formatting XML.cpp/format.h
@@ -0,0 +1,13 @@
+#ifndef FORMAT_H
+#define FORMAT_H
+
+#include <string>
+#include <vector>
+
+std::string generateIndentation(const std::string& space, int level);
+std::vector<std::string> tokenizeXML(const std::string& xmlContent);
+std::string processXML(const std::string& xmlContent, const std::string& outputFilePath);
+std::string readXMLFromFile(const std::string& filePath);
+void writeXMLToFile(const std::string& xmlContent, const std::string& filePath);
+
+#endif
