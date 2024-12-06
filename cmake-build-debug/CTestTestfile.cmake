63176879e116262b71d0d06918e7b23856fe16b7
Abdelrahman Sherif <abdelrahmansherif599@gmail.com>
2024-12-06 07:15:53 +0200

    adding new branch

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: D:/Data Faculty/Data Structures and Algorithms/New folder/xml-parser-cpp
+# Build directory: D:/Data Faculty/Data Structures and Algorithms/New folder/xml-parser-cpp/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(XML_CONSISTENCY_CHECK "D:/Data Faculty/Data Structures and Algorithms/New folder/xml-parser-cpp/cmake-build-debug/xml_consistency_check.exe")
+set_tests_properties(XML_CONSISTENCY_CHECK PROPERTIES  _BACKTRACE_TRIPLES "D:/Data Faculty/Data Structures and Algorithms/New folder/xml-parser-cpp/CMakeLists.txt;20;add_test;D:/Data Faculty/Data Structures and Algorithms/New folder/xml-parser-cpp/CMakeLists.txt;0;")
+add_test(XML_TO_JSON_CHECK "D:/Data Faculty/Data Structures and Algorithms/New folder/xml-parser-cpp/cmake-build-debug/xml_to_json.exe")
+set_tests_properties(XML_TO_JSON_CHECK PROPERTIES  _BACKTRACE_TRIPLES "D:/Data Faculty/Data Structures and Algorithms/New folder/xml-parser-cpp/CMakeLists.txt;23;add_test;D:/Data Faculty/Data Structures and Algorithms/New folder/xml-parser-cpp/CMakeLists.txt;0;")
+add_test(XML_TO_TREE_CHECK "D:/Data Faculty/Data Structures and Algorithms/New folder/xml-parser-cpp/cmake-build-debug/xml_to_tree.exe")
+set_tests_properties(XML_TO_TREE_CHECK PROPERTIES  _BACKTRACE_TRIPLES "D:/Data Faculty/Data Structures and Algorithms/New folder/xml-parser-cpp/CMakeLists.txt;26;add_test;D:/Data Faculty/Data Structures and Algorithms/New folder/xml-parser-cpp/CMakeLists.txt;0;")
+add_test(STRING_FORMATTING_CHECK "D:/Data Faculty/Data Structures and Algorithms/New folder/xml-parser-cpp/cmake-build-debug/string_formatting.exe")
+set_tests_properties(STRING_FORMATTING_CHECK PROPERTIES  _BACKTRACE_TRIPLES "D:/Data Faculty/Data Structures and Algorithms/New folder/xml-parser-cpp/CMakeLists.txt;29;add_test;D:/Data Faculty/Data Structures and Algorithms/New folder/xml-parser-cpp/CMakeLists.txt;0;")
