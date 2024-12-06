e4a525e3a7c3a737f077dd43db2bd3ec3496f3e2
OmarTamer2 <167299851+OmarTamer2@users.noreply.github.com>
2024-12-06 20:22:03 +0200

    Adding fix_errors

--- src/core/consistency/consistency.h
+++ src/core/consistency/consistency.h
@@ -9,5 +9,7 @@ int validateXML(std::vector<std::string> &tags);
 int report_errors(std::vector <std::string> &lines, std::vector <std::string> &tags, int error_index);
 void stringToLines (std::string &XML, std::vector <std::string> &LinedXml);
 int check_errors(std::string xml_file);
+int find_errors(std::vector <std::string> &lines, std::vector <std::string> &tags, int error_index);
+int fix_errors(std::string xml_file);
 
 #endif //XML_PARSER_CONSISTENCY_H
