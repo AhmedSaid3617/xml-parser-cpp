c123eb8b89567f13e24e06ec7ed019613f7f9993
OmarTamer2 <167299851+OmarTamer2@users.noreply.github.com>
2024-12-06 20:23:48 +0200

    Adding function definitions

--- src/core/consistency/functions.cpp
+++ src/core/consistency/functions.cpp
@@ -114,6 +114,90 @@ void stringToLines (string &XML, vector <string> &LinedXml){
     }
 }
 
+int find_errors(vector <string> &lines, vector <string> &tags, int error_index) {
+    int j, occur = 0;
+    string tag;
+    int starting = 0;
+    bool duplicate = false;
+    if(error_index == -1) return -1;
+    string error = tags[error_index];
+    if(tags[error_index] == tags[error_index+1] || tags[error_index] == tags[error_index - 1]) duplicate = true;
+    for (int s = 0; s < error_index; s++) {
+        if (tags[s] == error) occur++;
+    }
+    for (int i = 0; i < lines.size(); i++) {
+        j = 0;
+        while (lines[i][j] == ' ') {
+            j++;
+        }
+        if (lines[i][j] != '<') continue;
+        starting = j;
+        while (lines[i][j] != '>') {
+            tag.push_back(lines[i][j]);
+            j++;
+        }
+        tag.push_back('>');
+        if (tag == error && occur == 0 && duplicate) {
+            if(error.at(1) == '/') {
+                lines[i].erase(lines[i].begin()+starting +1);
+            }
+            else if(error.at(1) != '/') {
+                lines[i].insert(lines[i].begin()+starting +1, '/');
+            }
+            return i+1;
+        }
+        if (tag == error && occur == 0) {
+            if(error.at(1) == '/') {
+                error.erase(error.begin()+1);
+                lines.insert(lines.begin()+ i, error);
+            }
+            else if(error.at(1) != '/') {
+                error.insert(error.begin()+1,'/');
+                cout<<error<<endl;
+                lines.insert(lines.begin()+ i+1, error);
+            }
+            return i+1;
+        }
+        if (tag == error) occur--;
+        tag.clear();
+        if(j != lines[i].size() - 1) {
+            while (lines[i][j] != '<') {
+                j++;
+            }
+            starting = j;
+            while (lines[i][j] != '>') {
+                tag.push_back(lines[i][j]);
+                j++;
+            }
+            tag.push_back('>');
+            if (tag == error && occur == 0 && duplicate) {
+                if(error.at(1) == '/') {
+                    lines[i].erase(lines[i].begin()+starting +1);
+                }
+                else if(error.at(1) != '/') {
+                    lines[i].insert(lines[i].begin()+starting +1, '/');
+                }
+                return i+1;
+            }
+            if (tag == error && occur == 0) {
+                if(error.at(1) == '/') {
+                    error.erase(error.begin()+1);
+                    lines.insert(lines.begin()+ i, error);
+                }
+                else if(error.at(1) != '/') {
+                    error.insert(error.begin()+1,'/');
+                    error.erase(error.begin()+1);
+                    lines.insert(lines.begin()+ i+1, error);
+                }
+                return i+1;
+            }
+            if (tag == error) occur--;
+            tag.clear();
+        }
+    }
+    return -1;
+}
+
 int check_errors(string xml_data) {
     int tag_index = 0;
     vector <string> lines;
@@ -126,4 +210,28 @@ int check_errors(string xml_data) {
     if(tag_index == -1) return -1;
 
     else return report_errors(lines, tags, tag_index);
+}
+
+string fix_errors(string xml_data) {
+    int tag_index = 0;
+    int error_index = 0;
+    string data = "";
+    vector <string> lines;
+    vector <string> tags;
+
+    tokenizeXML(xml_data, tags);
+    stringToLines(xml_data, lines);
+    tag_index = validateXML(tags);
+    error_index = find_errors(lines, tags, tag_index);
+    if(error_index == -1) {
+        return xml_data;
+    }
+    else {
+        data+=lines[0];
+        for(int i = 1; i < lines.size(); i++) {
+            data+= '\n';
+            data += lines[i];
+        }
+        return fix_errors(data);
+    }
 }
