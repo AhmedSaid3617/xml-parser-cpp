52a1e3b21bdceadb32f90347db4b71235979a8e5
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-12-08 14:21:30 +0200

    Fixed minor errors and memory safety issues.

--- src/core/convert/Conversion_Function.cpp
+++ src/core/convert/Conversion_Function.cpp
@@ -7,6 +7,8 @@ string xml_to_json(string xml)
     auto * converter = new XML_To_Tree2(xml);
     TreeNode * root  = converter->convert();
     string output= XML_To_Tree2::convert_json(root->children[0]);
+    delete root;
+    delete converter;
     return output;
 }
 
