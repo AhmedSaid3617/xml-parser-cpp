2042c76a7a661e38d0bba83345637b44828927ba
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-12-13 13:47:35 +0200

    Added general structure for a command-line tool.

--- src/console/main.cpp
+++ src/console/main.cpp
@@ -1,10 +1,33 @@
 #include <string>
+#include <vector>
+#include <algorithm>
 #include <iostream>
 #include "fromat/format.h"
 
-int main() {
-    std::string my_string = "<id><x></x></id>";
-    std::cout << formatXML(my_string);
+using namespace std;
+
+vector<string> options = {"format", "compress"};
+
+
+void printUsage() {
+    std::cout << "Usage: xml_editor <option> -i <input_file> -o <output_file>\n";
+}
+
+int main(int argc, char *argv[]) {
+    
+    if (argc < 5)
+    {
+        printUsage();
+        return 1;
+    }
+
+    for (int i = 0; i < argc; i++)
+    {
+        if (std::find(options.begin(), options.end(), argv[i]) != options.end())
+        {
+            
+        }
+    }
 
     return 0;
 }
