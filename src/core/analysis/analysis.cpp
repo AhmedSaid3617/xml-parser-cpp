4a9c87f8be0352ea42a8abc03502a7a47e6423e8
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-12-15 15:01:42 +0200

    Added function signatures.

--- src/core/analysis/analysis.cpp
+++ src/core/analysis/analysis.cpp
@@ -1,1 +1,11 @@
 #include "analysis.h"
+
+User *get_most_influential(SocialNetwork &social_network)
+{
+    return nullptr;
+}
+
+User *get_most_active(SocialNetwork &social_network)
+{
+    return nullptr;
+}
--- src/core/analysis/analysis.h
+++ src/core/analysis/analysis.h
@@ -1,6 +1,12 @@
 #ifndef ANALYSIS_H
 #define ANALYSIS_H
 
+#include <vector>
 
+#include "data/User.h"
+#include "graph/SocialNetwork.h"
+
+User* get_most_influential(SocialNetwork &social_network);
+User* get_most_active(SocialNetwork &social_network);
 
 #endif
