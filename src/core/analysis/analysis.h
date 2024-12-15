086de6261517c0d50cf9c1c6065ba82d4b2aa4ef
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-12-15 15:04:48 +0200

    Added function signatures.

--- src/core/analysis/analysis.h
+++ src/core/analysis/analysis.h
@@ -1,6 +1,12 @@
 #ifndef ANALYSIS_H
 #define ANALYSIS_H
 
+#include <vector>
 
+#include "data/User.h"
+#include "graph/SocialNetwork.h"
+
+std::vector<User> get_mutual(SocialNetwork &social_network, std::vector<User> &users);
+std::vector<User> get_suggested(SocialNetwork &social_network, User &user);
 
 #endif
