dd6a5dc9d98948e7fae9186f90a0f48423dbd460
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-12-27 17:21:31 +0200

    Added test not working.

--- src/core/compression/compression.h
+++ src/core/compression/compression.h
@@ -1,18 +1,38 @@
 #ifndef COMRESSION_H
 #define COMRESSION_H
 
-string readFile(const string &filename);
-string minifyXML(const string &xmlContent);
-unordered_map<string, int> calculateFrequencies(const string &text);
-string replaceFrequentPairs(const string &text, const string &pair, const string &token);
-pair<string, unordered_map<string, string>> compress(const string &text);
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <regex>
+#include <cassert>
+#include<stack>
+
+
+using namespace std;
+
+struct CompressionResult {
+    string compressedText;
+    unordered_map<string, string> mapping;
+    stack<string> tokens;
+};
+
+std::string readFile(const std::string &filename);
+std::string minifyXML(const std::string &xmlContent);
+unordered_map<std::string, int> calculateFrequencies(const std::string &text);
+std::string replaceFrequentPairs(const std::string &text, const std::string &pair, const std::string &token);
+CompressionResult compress(const string &text);
 string tokenExisit(const string &compressedText, const unordered_map<string, string> &mapping);
-string replaceToken(const string &compressedText, const unordered_map<string, string> &mapping);
-string decompressing(const string &compressedText, const unordered_map<string, string> &mapping);
+string replaceToken(const string &compressedText, const unordered_map<string, string> &mapping,string _token);
+string decompressing(const string &compressedText, const unordered_map<string, string> &mapping,stack<string>& tokens);
 void displayHashTable(unordered_map<string,auto> freq);void createFile(const string& filename, const string& text, const unordered_map<string, string>& data, stack<string>& stackData);
 CompressionResult loadFile(const string& filename);
 void writeStringToFile(const std::string& filename, const std::string& text);
 CompressionResult loadFile(const string& filename) ;
 string Decompress(string text);
 string Compress(string text);
+
 #endif
--- test/xml_compression.cpp
+++ test/xml_compression.cpp
@@ -1,5 +1,94 @@
 #include "compression/compression.h"
 #include <cassert>
-int main(){
-    assert(true);
+int main()
+{
+    string my_xml = R"(
+    <users>
+    <user>
+        <id>1</id>
+        <name>Ahmed Ali</name>
+        <posts>
+            <post>
+                <body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </body>
+                <topics>
+                    <topic>
+                        economy
+                    </topic>
+                    <topic>
+                        finance
+                    </topic>
+                </topics>
+            </post>
+            <post>
+                <body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </body>
+                <topics>
+                    <topic>
+                        solar_energy
+                    </topic>
+                </topics>
+            </post>
+        </posts>
+        <followers>
+            <follower>
+                <id>2</id>
+            </follower>
+            <follower>
+                <id>3</id>
+            </follower>
+        </followers>
+    </user>
+    <user>
+        <id>2</id>
+        <name>Yasser Ahmed</name>
+        <posts>
+            <post>
+                <body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </body>
+                <topics>
+                    <topic>
+                        education
+                    </topic>
+                </topics>
+            </post>
+        </posts>
+        <followers>
+            <follower>
+                <id>1</id>
+            </follower>
+        </followers>
+    </user>
+    <user>
+        <id>3</id>
+        <name>Mohamed Sherif</name>
+        <posts>
+            <post>
+                <body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </body>
+                <topics>
+                    <topic>
+                        sports
+                    </topic>
+                </topics>
+            </post>
+        </posts>
+        <followers>
+            <follower>
+                <id>1</id>
+            </follower>
+        </followers>
+    </user>
+</users>
+    )";
+
+    string compressed_xml = Compress(my_xml);
+
+    string decompressed_xml = Decompress(compressed_xml);
+
+    cout << decompressed_xml;
 }
