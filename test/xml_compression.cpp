e9646c49b53ac23d7f6cb025e1707bdd6e59c9c3
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-12-28 15:27:38 +0200

    Decompression test passes.

--- test/xml_compression.cpp
+++ test/xml_compression.cpp
@@ -1,14 +1,48 @@
 #include "compression/compression.h"
 #include <cassert>
 #include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
 
+using namespace std;
 
+int main()
+{
 
-int main(){
-    auto result = Compress(minifyXML("<html>\t"
-                                     ""
-                                     ""
-                                     ""
-                                     "<pot>test test test</pot></html>"));
-    std::cout << Decompress(result);
+    string xml1 = "<html>\t"
+                  "\n"
+                  "\n"
+                  ""
+                  "<pot>test test test</pot></html>";
+
+    string compressed = Compress(minifyXML(xml1));
+    string decompressed = Decompress(compressed);
+    assert(decompressed == minifyXML(xml1));
+
+    // IMPORTANT: file path depends on where you're running the bin/exe from.
+    std::ifstream file("./test/sample.txt");
+    std::ostringstream buffer;
+    buffer << file.rdbuf();
+    file.close();
+
+    // Compress into out.txt
+    string xml2 = buffer.str();
+    std::ofstream file_out("out.txt");
+    file_out << Compress(minifyXML(xml2));
+    file_out.close();
+
+    // Decompress from out.txt
+    std::ifstream file_in("out.txt");
+    std::ostringstream buffer1;
+    buffer1 << file_in.rdbuf();
+    file_in.close();
+
+    decompressed = Decompress(buffer1.str());
+    //cout << xml2 << endl;
+    cout << decompressed;
+    assert(decompressed == minifyXML(xml2));
+
+    return 0;
 }
